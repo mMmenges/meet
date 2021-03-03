fe828f6c381cce4cb40fc4956892463256772ef8
Michael Menges <michael.menges.webdev@gmail.com>
2021-03-04 00:56:09 +0100

    task 4.9

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import React, { Component } from "react";
+import "./App.css";
 import CitySearch from "./CitySearch";
 import EventList from "./EventList";
-import "./App.css";
 import NumberOfEvents from "./NumberOfEvents";
 import { getEvents } from "./api";
 import "./nprogress.css";
 import { WarningAlert } from "./Alert";
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import EventGenre from "./EventGenre";
 
 class App extends Component {
   state = {
     events: [],
-    scurrentLocation: "all",
+    currentLocation: "all",
     locations: [],
     numberOfEvents: 32,
   };
 
-  getData = () => {
-    const { locations, events } = this.state;
-    const data = locations.map((location) => {
-      const number = events.filter((event) => event.location === location).length
-      const city = location.split(' ').shift()
-      return { city, number };
-    })
-    return data;
-  };
-
-
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
+    if (!navigator.onLine) {
+      this.setState({
+        warningText:
+          "As you are offline, you are only viewing the cached events from your last session",
+      });
+    }
     getEvents().then((response) => {
       if (this.mounted) {
         this.setState({
           events: response.events,
-          locations: response.locations
+          locations: response.locations,
         });
       }
     });
@@ -42,7 +39,17 @@ class App extends Component {
     this.mounted = false;
   }
 
-  
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location)
+        .length;
+      const city = location.split(" ").shift();
+      return { city, number };
+    });
+    return data;
+  };
+
   updateEvents = (location, eventCount) => {
     const { currentLocation, numberOfEvents } = this.state;
     if (location) {
@@ -77,16 +84,42 @@ class App extends Component {
   };
 
   render() {
+    const { locations, numberOfEvents, events } = this.state;
     return (
       <div className="App">
-         <WarningAlert text={this.state.warningText} />
-         <h1>Meet App</h1>
+        <WarningAlert text={this.state.warningText} />
+        <h1>Meet App</h1>
         <h4>Choose your nearest city</h4>
-        <CitySearch
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
+        <NumberOfEvents
           updateEvents={this.updateEvents}
-          locations={this.state.locations}
+          numberOfEvents={numberOfEvents}
         />
-        <NumberOfEvents updateEvents={this.updateEvents} />
+        <h4>Events in each city</h4>
+        <div className="data-vis-wrapper">
+          <EventGenre events={events} />
+          <ResponsiveContainer height={400}>
+            <ScatterChart
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis
+                allowDecimals={false}
+                type="number"
+                dataKey="number"
+                name="Number Of Events"
+              />
+              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList events={this.state.events} />
       </div>
     );
