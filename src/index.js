8742ade41dfed9b6e19a15daa665ce172bb67805
Michael Menges <michael.menges.webdev@gmail.com>
2021-03-03 09:21:51 +0100

    <link rel=apple-touch-icon href=%PUBLIC_URL%/citymeet-512.png />

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-// import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 import * as atatus from 'atatus-spa';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -12,6 +12,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-// serviceWorker.register();
+//serviceWorker.register();
 atatus.config('9d2555348474414d9ea27bcd7b0381fd').install();
 //atatus.notify(new Error('Test Atatus Setup'));
